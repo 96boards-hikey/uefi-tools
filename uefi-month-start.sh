da88d34a5b7c9d604cbe3c8ad6d577f211d69db9
Ryan Harkin <ryan.harkin@linaro.org>
2013-03-26 14:59:34 +0000

    uefi-month-start: set default month tag
    Signed-off-by: Ryan Harkin <ryan.harkin@linaro.org>

--- uefi-month-start.sh
+++ uefi-month-start.sh
@@ -21,7 +21,7 @@ function usage
 # Check all the parameters
 # we should pass in the YYYY.MM used for the release, eg. "2013.01" for January 2013
 
-$YYYYMM=`date +%Y.%m`
+YYYYMM=`date +%Y.%m`
 
 while [ "$1" != "" ]; do
     case $1 in
