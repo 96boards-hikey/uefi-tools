7e4ec3047e4f09fd66719d4f6686b40e388b827f
Ryan Harkin <ryan.harkin@linaro.org>
2013-05-08 18:50:52 +0100

    uefi-backup-branches: add prefix parameter
    Allow the use to supply 1 parameter to act as a prefix to the backup branch names.
    
    Before this change, all backup branches were prefixed with "previous-".  Now, "previous-" is only the default prefix, should one not be provided.
    
    Note: the user does not provide the "-" in the prefix name, eg:
    
      uefi-backup-branches.sh someprefix
    
    Will create backup branches like someprefix-linaro-tracking.
    
    Signed-off-by: Ryan Harkin <ryan.harkin@linaro.org>

--- uefi-backup-branches.sh
+++ uefi-backup-branches.sh
@@ -11,13 +11,24 @@
 ################################################################################
 function usage
 {
-	echo "Usage: $0"
+	echo "Usage: $0 <prefix>"
 }
 ################################################################################
 
+if [ "$1" = "" ]
+then
+	PREFIX="previous"
+	echo "Using default prefix: $PREFIX"
+else
+	PREFIX=$1
+    echo "PREFIX is $PREFIX"
+fi
+
+
+
 current_branch=`git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
 branches=(
-	`git branch --list linaro-topic-* | sed "s/*//"`
+	`git branch --list linaro-topic-* linaro-internal-feature-* linaro-internal-topic-* | sed "s/*//"`
 	master
 	armlt-tracking
 	linaro-tracking
@@ -28,10 +39,10 @@ branches=(
 
 for branch in "${branches[@]}" ; do
 	echo "--------------------------------------------------------------------------------"
-	echo "Backing up $branch to previous-$branch"
-	git branch -D previous-$branch
+	echo "Backing up $branch to $PREFIX-$branch"
 	git checkout $branch
-	git branch previous-$branch
+	git branch -D $PREFIX-$branch
+	git branch $PREFIX-$branch $branch
 
 	if [ "$?" != "0" ]
 	then
