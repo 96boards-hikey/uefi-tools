525062e67a86fb17039d5cacd2455a72e203005e
Ryan Harkin <ryan.harkin@linaro.org>
2013-05-14 19:06:26 +0100

    uefi-rtsm: only set env vars if not already set
    Signed-off-by: Ryan Harkin <ryan.harkin@linaro.org>

--- uefi-rtsm.sh
+++ uefi-rtsm.sh
@@ -15,16 +15,29 @@ then
 	echo "FlexLm tunnel not running, starting it...."
 
 	# Run the license pass-through
-	ssh -L 8224:localhost:8224 -L 18224:localhost:18224 -N portfwd@23.21.178.170 &
+	#ssh -L 8224:localhost:8224 -L 18224:localhost:18224 -N portfwd@23.21.178.170 &
+    gnome-terminal --geometry=32x4 --title "flexlm tunnel" -e "ssh -L 8224:localhost:8224 -L 18224:localhost:18224 -N ryan.harkin@flexlm.linaro.org -v"
+
 fi
 
 # add RTSM stuff to your env
 source $HOME/ARM/FastModelsTools_7.1/source_all.sh
 
 # add a variable for the model you want
-RTSM_MODEL=$HOME/ARM/RTSM/Linux64_RTSM_VE_Cortex-A15x2/RTSM_VE_Cortex-A15x2
+if [ "$RTSM_MODEL" == "" ]
+then
+	RTSM_MODEL=$HOME/ARM/RTSM/Linux64_RTSM_VE_Cortex-A15x2/RTSM_VE_Cortex-A15x2
+fi
 
-RTSM_UEFI=$WORKSPACE/Build/ArmVExpress-RTSM-A15_MPCore/DEBUG_ARMLINUXGCC/FV/RTSM_VE_CORTEX-A15_MPCORE_EFI.fd
+if [ "$RTSM_UEFI" == "" ]
+then
+	RTSM_UEFI=$WORKSPACE/Build/ArmVExpress-RTSM-A15_MPCore/DEBUG_ARMLINUXGCC/FV/RTSM_VE_CORTEX-A15_MPCORE_EFI.fd
+fi
+
+if [ "$RTSM_MMC" == "" ]
+then
+	RTSM_MMC=/linaro/releases/ubuntu-1304/vexpress-quantal_developer_20130423-299.img
+fi
 
 # Run the model
 $RTSM_MODEL \
