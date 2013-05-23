2c65b6b3a0eb9c2a165f5813cdb2e461d01697a2
Ryan Harkin <ryan.harkin@linaro.org>
2013-05-23 18:24:58 +0100

    uefi-month-end.sh: don't update linaro-tracking
    linaro-tracking and armlt-tracking are updated each time a release candidate is generated, so we don't need to update them again here.
    
    Signed-off-by: Ryan Harkin <ryan.harkin@linaro.org>

--- uefi-month-end.sh
+++ uefi-month-end.sh
@@ -5,8 +5,7 @@
 # At the end of the month, we:
 #
 # 1) Merge the current monthly branch to the release branch
-# 2) Make sure the linaro/armlt-tracking branches are up-to-date
-# 3) Tag the release branch with linaro-uef-YYYY.MM
+# 2) Tag the release branch with linaro-uef-YYYY.MM
 ################################################################################
 
 
@@ -19,13 +18,4 @@ echo "Updating linaro-release"
 echo "--------------------------------------------------------------------------------"
 force_update_branch $(uefi_next_current_month_branch) linaro-release
 git tag $(uefi_next_release_tag)
-
-echo "--------------------------------------------------------------------------------"
-echo "Update global tracking branches"
-echo "--------------------------------------------------------------------------------"
-force_update_branch $(uefi_next_current_month_branch) linaro-tracking
-
-# Force armlt-tracking to match linaro-tracking exactly
-git branch -D armlt-tracking
-git branch armlt-tracking linaro-tracking
 
