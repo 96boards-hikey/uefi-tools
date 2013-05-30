d682c0a1b31d40d9df1e8ff375bfaa579c180e1a
Ryan Harkin <ryan.harkin@linaro.org>
2013-05-30 15:50:53 +0100

    uefi-save-patches.sh: use uefi-common
    Update the script to use the uefi-common functions rather than providing its own methods to extract data from the tree.
    
    Signed-off-by: Ryan Harkin <ryan.harkin@linaro.org>

--- uefi-save-patches.sh
+++ uefi-save-patches.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
+################################################################################
+source uefi-common
+################################################################################
+cd $UEFI_NEXT_GIT
 
+# save current branch so we can return to it after saving the patches
 curr_branch=`git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
 
-version=`git tag --list linaro-uefi-*-rc* | tail -1`
-[[ "$version" =~ (.*[^0-9])([0-9]+)$ ]] && version="${BASH_REMATCH[1]}$((${BASH_REMATCH[2]} + 1))";
-RC=`echo $version | sed 's#linaro-uefi-.*-rc##g'`
-YYYYMM=`echo $version | sed 's#linaro-uefi-##g' | sed 's#-rc.*##g'`
+if [ "$1" == "" ]
+then
+	PATCH_DIR=$UEFI_SAVE_PATCHES_DIR/$(uefi_next_get_YYYYMM)
+else
+	PATCH_DIR=$UEFI_SAVE_PATCHES_DIR/$1
+fi
 
-BASE_COMMIT=`git log -1 linaro-base-$YYYYMM | head -1 | sed 's/commit //'`
+BASE_TAG=`git tag --list linaro-base-* | tail -1`
+BASE_COMMIT=`git log -1 $BASE_TAG | head -1 | sed 's/commit //'`
 
 topics=(`git branch --list linaro-topic-* | sed "s/*//"`)
 
 for topic in "${topics[@]}" ; do
 
-	SAVE_DIR=/linaro/uefi/master/patches/$YYYYMM/$topic
+	SAVE_DIR=$PATCH_DIR/$topic
 
 	# update monthly branch
 	# Now that we have the topic branches, we merge them all back to the tracking branch
